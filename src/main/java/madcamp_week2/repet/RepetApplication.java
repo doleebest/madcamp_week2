9ae1e63a11ecf5ee9dfcb499f977e392e91fda01
doleebest <sj27419@gmail.com>
2025-01-02 21:08:53 +0900

    init

---
+++ src/main/java/madcamp_week2/repet/RepetApplication.java
@@ -0,0 +1,13 @@
+package madcamp_week2.repet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepetApplication.class, args);
+	}
+
+}
