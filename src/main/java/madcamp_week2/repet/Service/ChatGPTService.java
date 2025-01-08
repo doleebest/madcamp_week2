363a13b69ff94b79e6f06021bf4ac884a39f25d6
doleebest <sj27419@gmail.com>
2025-01-08 17:37:47 +0900

    feat : gpt train

--- src/main/java/madcamp_week2/repet/Service/ChatGPTService.java
+++ src/main/java/madcamp_week2/repet/Service/ChatGPTService.java
@@ -1,7 +1,9 @@
 package madcamp_week2.repet.Service;
 
 import madcamp_week2.repet.Domain.Board;
+import madcamp_week2.repet.Domain.ChatMessage;
 import madcamp_week2.repet.Repository.BoardRepository;
+import madcamp_week2.repet.Repository.ChatMessageRepository;
 import madcamp_week2.repet.Domain.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -23,6 +25,9 @@ public class ChatGPTService {
     @Autowired
     private BoardRepository boardRepository;
 
+    @Autowired
+    private ChatMessageRepository chatMessageRepository;
+
     private final RestTemplate restTemplate;
 
     @Value("${chatgpt.api.key}")
@@ -41,6 +46,16 @@ public class ChatGPTService {
                 .collect(Collectors.joining());
     }
 
+    private String getChatMessage(List<ChatMessage> messages) {
+        StringBuilder re = new StringBuilder();
+        messages.stream()
+                .forEach(chatMessage -> {
+                    re.append(chatMessage.getUserMessage());
+                    re.append(chatMessage.getPetResponse());
+                });
+        return re.toString();
+    }
+
     public String generatePetResponse(Pet pet, String userMessage) {
         // 펫 정보를 포함한 프롬프트 생성
         String petInfo = String.format(
@@ -55,6 +70,7 @@ public class ChatGPTService {
                         "- 장점: %s\n" +
                         "- 단점: %s\n\n" +
                         "게시판에 작성된 우리의 추억들:\n%s\n\n" + // boards의 content 추가
+                        "- 지난 대화 이력: %s\n\n" +
                         "이러한 특성들을 모두 반영하여, 실제 이 반려동물의 성격과 경험을 바탕으로 대화해주세요. " +
                         "특히 추억과 사건들을 간간히 언급하며 더 현실감 있게 대화해주세요. "+
                         "그리고 주인이 펫 로스 증후군을 앓고 있다는 점을 고려하여 부드럽게, 자연스럽게 대화해주세요." +
@@ -70,7 +86,8 @@ public class ChatGPTService {
                 pet.getMishap(),
                 pet.getStrengths(),
                 pet.getWeaknesses(),
-                getBoardContents(boardRepository.findByUserId(pet.getUser().getEmail()))
+                getBoardContents(boardRepository.findByUserId(pet.getUser().getEmail())),
+                getChatMessage(chatMessageRepository.findByPetIdOrderByCreatedAtDesc(pet.getId()))
         );
         System.out.println(petInfo);
 
